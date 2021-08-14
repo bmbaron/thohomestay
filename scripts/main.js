333197e67db7f7da246dbdcdf05ed0508052cb0d
unknown <benjamin.m.baron@gmail.com>
2021-08-14 15:48:12 +0700

    small changes

--- scripts/main.js
+++ scripts/main.js
@@ -7,18 +7,10 @@ let images = [
     '../thohomestay/images/6.png',
 ];
 
-let mainImage = '../images/top-main.png';
-
 let photos = document.getElementsByClassName("examples");
 for (let i = 0; i < photos.length; i++) {
     photos[i].src = images[i];
 }
-
-
-
-
-let imageCounter = 0;
-
 
 const review = {
     content: "",
@@ -29,17 +21,17 @@ const review = {
 const leftReview = Object.create(review);
 leftReview.content = "&quot;If you want to experience something really local and the kindness of the Vietnamese, don't miss out on this beautiful experience!&quot;";
 leftReview.name = "Jordane G.";
-leftReview.photo = "../images/reviewers/jordane.png";
+leftReview.photo = "../thohomestay/images/reviewers/jordane.png";
 
 const centerReview = Object.create(review);
 centerReview.content = "&quot;The homestay is absolutely great, the hospitality, food, cleanliless everything exceeded our expections.&quot;";
 centerReview.name = "Trinh S.";
-centerReview.photo = "../images/reviewers/trinh.png";
+centerReview.photo = "../thohomestay/images/reviewers/trinh.png";
 
 const rightReview = Object.create(review);
 rightReview.content = "&quot;A truly authentic experience. The family is lovely and really look after you. Get there before tourism kicks in.&quot;";
 rightReview.name = "Loretta I.";
-rightReview.photo = "../images/reviewers/loretta.png";
+rightReview.photo = "../thohomestay/images/reviewers/loretta.png";
 
 const reviewObjects = [leftReview,centerReview,rightReview];
 
@@ -58,41 +50,6 @@ for (let i = 0; i < reviews.length; i++) {
 let reviewButton = document.getElementById("review-button");
 reviewButton.onclick = function () {
     window.open('https://www.google.com/maps/place/Tho+Homestay+and+Tea+Ceremony/@22.4981703,104.5267565,18z/data=!4m10!3m9!1s0x36ccee48eb8f3619:0xb03e740dab8648a1!5m2!4m1!1i2!8m2!3d22.4974492!4d104.5274056!9m1!1b1');
-}
-
-
-
-
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-  
-
-let slideshow = document.getElementsByClassName('slideshow')[0];
-
-/* window.onload(changeImage());
-
-function changeImage() {
-
-    if(imageCounter == images.length) {
-        imageCounter = 0;
-    }
-    slideshow.style.backgroundImage = "url('" + images[imageCounter] + "')";
-    imageCounter++;
-
-    changeSlides();
-    
-} */
-
-
-async function changeSlides() {
-
-    slideshow.classList.toggle('slideCenter');
-    await sleep (5000);
-    slideshow.classList.toggle('slideCenter');
-    changeImage();
-
 }
 
 
