7c33e6c99f3d26de8e9e42d3e3d3509dc9ff991d
unknown <benjamin.m.baron@gmail.com>
2021-08-22 22:25:09 +0700

    added intense.js library and made image organization changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
+function toggleMobileMenu() {
+        document.getElementById('nav-menu').classList.toggle('active');
+        document.getElementsByClassName('mobile-bar')[0].classList.toggle('active');
+} 
+
 let images = [
     'https://raw.githubusercontent.com/bmbaron/thohomestay/master/images/1.png',
     'https://raw.githubusercontent.com/bmbaron/thohomestay/master/images/2.png',
@@ -15,27 +20,28 @@ for (let i = 0; i < photos.length; i++) {
 const review = {
     content: "",
     name: "",
-    photo: ""
+    photo: "",
+    alt: ""
 };
 
 const leftReview = Object.create(review);
 leftReview.content = "&quot;If you want to experience something really local and the kindness of the Vietnamese, don't miss out on this beautiful experience!&quot;";
 leftReview.name = "Jordane G.";
 leftReview.photo = "https://raw.githubusercontent.com/bmbaron/thohomestay/master/images/reviewers/jordane.png";
-leftReview.photo.alt = "reviewer Jordane G.";
+leftReview.alt = "reviewer Jordane G.";
 
 const centerReview = Object.create(review);
 centerReview.content = "&quot;The homestay is absolutely great, the hospitality, food, cleanliless everything exceeded our expections.&quot;";
 centerReview.name = "Trinh S.";
 centerReview.photo = "https://raw.githubusercontent.com/bmbaron/thohomestay/master/images/reviewers/trinh.png";
-centerReview.photo.alt = "reviewer Trinh S.";
+centerReview.alt = "reviewer Trinh S.";
 
 
 const rightReview = Object.create(review);
 rightReview.content = "&quot;A truly authentic experience. The family is lovely and really look after you. Get there before tourism kicks in.&quot;";
 rightReview.name = "Loretta I.";
-rightReview.photo = "https://raw.githubusercontent.com/bmbaron/thohomestay/master/images/reviewers";
-rightReview.photo.alt = "reviewer Loretta I.";
+rightReview.photo = "https://raw.githubusercontent.com/bmbaron/thohomestay/master/images/reviewers/loretta.png";
+rightReview.alt = "reviewer Loretta I.";
 
 
 const reviewObjects = [leftReview,centerReview,rightReview];
@@ -47,6 +53,7 @@ for (let i = 0; i < reviews.length; i++) {
     let name = document.createElement('h3');
     let review = document.createElement('h6');
     image.src = reviewObjects[i].photo;
+    image.alt = reviewObjects[i].alt;
     name.innerText = reviewObjects[i].name;
     review.innerHTML = reviewObjects[i].content;
     reviews[i].append(image, name, review);
@@ -57,7 +64,10 @@ reviewButton.onclick = function () {
     window.open('https://www.google.com/maps/place/Tho+Homestay+and+Tea+Ceremony/@22.4981703,104.5267565,18z/data=!4m10!3m9!1s0x36ccee48eb8f3619:0xb03e740dab8648a1!5m2!4m1!1i2!8m2!3d22.4974492!4d104.5274056!9m1!1b1');
 }
 
-
+window.onload = function() {
+    var elements = document.querySelectorAll( '.examples' );
+    Intense( elements );
+}
 
 
 
